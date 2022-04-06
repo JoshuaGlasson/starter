8229f3f1eb495e61c1dded677e52cd652197d40b
JoshuaGlasson <glazzy5@gmail.com>
2022-04-06 18:46:04 +0100

    example commit

---
+++ js.js
@@ -0,0 +1,18 @@
+function doMaths(num) {
+    console.log("I did some maths!");
+
+    switch (num) {
+        case 0:
+            console.log("3 + 3 = " + (3 + 3));
+            break;
+        case 1:
+            console.log("3 x 3 = " + (3 * 3));
+            break;
+        case 2:
+            console.log("3 / 3 = " + (3 / 3));
+            break;
+        case 3:
+            console.log("3 - 3 = " + (3 - 3));
+            break;
+    }
+}
